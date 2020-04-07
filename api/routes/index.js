0c646f2798429d2e208fa291ce11685d215f7410
Francisco Quintana <franqg99@gmail.com>
2020-04-07 10:10:00 +0100

    initial commit

---
+++ api/routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+
+const usersRouter = require('./users.router')
+const authRouter = require('./auth.router')
+const { authUser } = require('../utils') // Authenticated Route
+
+router.use('/users', usersRouter)
+router.use('/auth', authRouter)
+
+router.get('/whoami', authUser, (req, res) => {
+  res.send(`hi there! ${res.locals.user.name}`)
+})
+
+module.exports = router

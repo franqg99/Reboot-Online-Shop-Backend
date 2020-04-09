92d3d0f9e3150beb89628acfdb9926818adc9723
Cristina Déniz <cdenizperdomo@gmail.com>
2020-04-10 00:56:19 +0100

    chaged controller
    Co-authored-by: Francisco Quintana González <franqg99@gmail.com>

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -4,7 +4,6 @@ const { handleError } = require('../utils')
 module.exports = {
   getProducts,
   createProduct
-  // getProductByQuery
 }
 
 function getProducts (req, res) {
@@ -15,19 +14,12 @@ function getProducts (req, res) {
   if (query === undefined) {
     ProductModel
       .find()
-      .then(product => res.json(product))
+      .then(products => res.json(products))
       .catch(err => handleError(err, res))
   } else {
     ProductModel
-      .find({ name: { $regex: query, $options: 'i' } })
-      .then(productsName => {
-        ProductModel
-          .find({ category: { $regex: query, $options: 'i' }})
-          .then(productsCategory => res.json({
-            name: productsName,
-            category: productsCategory
-          }))
-      })
+      .find({ $or: [{ category: { $regex: query, $options: 'i' } }, { name: { $regex: query, $options: 'i' } }] })
+      .then(products => res.json(products))
       .catch(err => handleError(err, res))
   }
 }
@@ -41,26 +33,3 @@ function createProduct (req, res) {
       res.status(403).json({ error: err })
     })
 }
-
-// function getProductByQuery (req, res) {
-//   const query = req.params.query
-//   ProductModel
-//     .find({ category: { $regex: query, $options: 'i' } })
-//     .then(categoryResponse => req.json(categoryResponse))
-// }
-// function searchProductByCategory (req, res) {
-//   const category = req.params.category
-//   ProductModel
-//     .find({ category: { $regex: category, $options: 'i' } })
-//     .then(categoryResult => res.json({ category: categoryResult }))
-//     .catch(err => handleError(err, res))
-// }
-
-// function searchProductBySubcategory (req, res) {
-//   const category = req.params.category
-//   const subcategory = req.params.subcategory
-//   ProductModel
-//     .find({ category: { $regex: category, $options: 'i' }, subcategory: { $regex: subcategory, $options: 'i' } })
-//     .then(subcategoryResult => res.json(subcategoryResult))
-//     .catch(err => handleError(err, res))
-// }
